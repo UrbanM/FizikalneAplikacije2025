b7d8bcda9bd60a8217fdcfac29ba3f31c14291ce
OPenworldgamer <kravosiztok2@gmail.com>
2025-03-11 19:49:40 +0100

    Pa še mal bolj natančno

--- doors bs.py
+++ doors bs.py
@@ -4,7 +4,7 @@ import random
 victories_c = int(0)
 victories_s = int(0)
 
-for _ in range(10000):
+for _ in range(100000):
 
     #Create 3 doors that are closed (0), choose one to have a prise (2)
     doors=[0,0,0]
@@ -39,8 +39,8 @@ for _ in range(10000):
     if doors[chosen_door]==2:
         victories_s += 1
 
-print("Winning percentage when switching doors: ", victories_c/10000*100, "%")
-print("Winning percentage when not switching doors: ", victories_s/10000*100, "%")
+print("Winning percentage when switching doors: ", victories_c/_*100, "%")
+print("Winning percentage when not switching doors: ", victories_s/_*100, "%")
 
     
 
